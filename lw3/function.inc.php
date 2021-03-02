376a2fb2e6bfe1c6e5380c503477b6f750604ca4
Grishw <57558364+Grishw@users.noreply.github.com>
2021-03-03 01:08:48 +0300

    add N3
    add N3 from lw3
    add function getCoutOfRepeatInString/isPassHaveAllowChar to function.inc.php

--- lw3/function.inc.php
+++ lw3/function.inc.php
@@ -1,13 +1,13 @@
 <?php
 
-    $stringOfLaterENGl = "abcdefghijklmnopqrstuvwxyz";
-    $stringOfLaterENGa = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    $stringOfNumber = "0123456789";
+    $stringOfLaterENGl = 'abcdefghijklmnopqrstuvwxyz';
+    $stringOfLaterENGa = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $stringOfNumber = '0123456789';
 
     function getGETParam (string $name) :?string
     {  
         return isset($_GET[$name]) ? ($_GET[$name]) : null;
-    };
+    }
     
     function getPosInLine(string $mainString, string $subString) :?int
     {
@@ -15,3 +15,42 @@
                         null : strpos($mainString, $subString);
     }
 
+    function isPassHaveAllowChar(string $pass, string $charSring, string $latterSringLower) 
+    {
+        $result = TRUE;
+        
+        for($i = 0; $i <= strlen($pass)-1; $i++)
+        {
+            
+            if(getPosInLine($charSring, $pass[$i]) === null)
+            { 
+                if(getPosInLine($latterSringLower, strtolower($pass[$i])) === null)
+                {
+                    $result = FALSE;
+                    break;
+                }
+            }
+            
+        }
+        
+        return $result;
+    }
+
+    function getCoutOfRepeatInString(string $pass): int
+    {
+        $result = 0;
+        $chekedCharString = "";
+        
+        for($i = 0; $i <= strlen($pass)-1; $i++)
+        {
+            if(getPosInLine($chekedCharString, $pass[$i]) === null)
+            {
+                substr_count($pass, $pass[$i]) === 1 ? : 
+                $result += substr_count($pass, $pass[$i]);
+                
+                $chekedCharString = $chekedCharString."$pass[$i]";
+            }
+             
+        }
+        return $result;
+    }
---
+++ lw3/passwordStrength.php
@@ -0,0 +1,56 @@
+<?php
+    header("Content-Type: text/plain");
+    require_once('function.inc.php');
+
+    $password = getGETParam("password");
+
+    $passStrength = 0;
+
+    if($password  === null)
+    {
+        echo "No password here";
+    }
+    else if( isPassHaveAllowChar($password, $stringOfNumber, $stringOfLaterENGl))
+    {
+        $size = strlen($password);
+        
+        $passStrength += 4*$size;
+        
+        for($i = 0; $i <= $size-1; $i++)
+        {
+            getPosInLine($stringOfNumber, $password[$i]) === null ? :  $passStrength += 4;
+        };
+        
+        $countOfApperLetter = 0;
+        for($i = 0; $i <= $size-1; $i++)
+        {
+            getPosInLine($stringOfLaterENGa, $password[$i]) === null ? :  $countOfApperLetter++;
+        };
+        
+        $countOfApperLetter === 0 ? : 
+        $passStrength += ($size - $countOfApperLetter)*2;
+        
+        $countOfLowerLetter = 0;
+        for($i = 0; $i <= $size-1; $i++)
+        {
+            getPosInLine($stringOfLaterENGl, $password[$i]) === null ? :  $countOfLowerLetter++;
+        };
+        
+        $countOfLowerLetter === 0 ? : 
+        $passStrength += ($size - $countOfLowerLetter)*2; 
+        
+        $coutOfLetter = $countOfLowerLetter + $countOfApperLetter;
+        $size - $coutOfLetter !== 0 ? : $passStrength -= $coutOfLetter;
+        $coutOfLetter !== 0 ? : $passStrength -= $size;
+        
+        $passStrength -= getCoutOfRepeatInString($password);
+
+        
+        echo "Pass power for $password = ".$passStrength;
+    }
+    else
+    {
+        echo "Haves some not allowed char";
+    };
+
+
